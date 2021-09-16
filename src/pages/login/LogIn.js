906e2f437775c832d6317564f322ae3ccfb9cc90
vineet-coder <vineetsinghal2312@gmail.com>
2021-09-16 14:53:00 +0530

    fix : login header

--- src/pages/login/LogIn.js
+++ src/pages/login/LogIn.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Header } from "../../components/header/Header";
+// import { Header } from "../../components/header/Header";
 import { SideNav } from "../../components/sideNav/SideNav";
 import { SmallLoader } from "../../components/smallLoader/smallLoader";
 import { useAuth } from "../../providers/AuthProvider";
@@ -101,7 +101,7 @@ export const LogIn = () => {
 
   return (
     <div className="log-in">
-      <Header />
+      {/* <Header /> */}
       <SideNav />
       <div className="contact-us">
         <form
